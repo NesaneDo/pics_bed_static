68ae1647b02d1ef64f27434bae84a0b241f40469
Rondo <1962741694@qq.com>
2021-08-24 12:08:02 +0800

    添加图片

--- js/img/getAlgorithmImgs.js
+++ js/img/getAlgorithmImgs.js
@@ -3,6 +3,8 @@ function getAlgorithmImgs(){
 	
 		// algorithm
 		{src: '/squares_of_a_sorted_array/1.png', desc: 'squares_of_a_sorted_array_1', type: 'algorithm'},
+		{src: '/remove_duplicates_from_sorted_list/1.jpg', desc: 'remove_duplicates_from_sorted_list_1', type: 'algorithm'},
+		{src: '/remove_duplicates_from_sorted_list/2.jpg', desc: 'remove_duplicates_from_sorted_list_2', type: 'algorithm'},
 		
     ]
 }
