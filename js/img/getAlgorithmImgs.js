0774dd4bf1064ee2e18772c1d34ee467784a3ca2
Rondo <1962741694@qq.com>
2021-08-30 10:20:02 +0800

    添加图片 algorithm.same_tree

--- js/img/getAlgorithmImgs.js
+++ js/img/getAlgorithmImgs.js
@@ -4,7 +4,9 @@ function getAlgorithmImgs(){
 		// algorithm
 		{src: '/squares_of_a_sorted_array/1.png', desc: 'squares_of_a_sorted_array_1', type: 'algorithm'},
 		{src: '/remove_duplicates_from_sorted_list/1.jpg', desc: 'remove_duplicates_from_sorted_list_1', type: 'algorithm'},
-		{src: '/remove_duplicates_from_sorted_list/2.jpg', desc: 'remove_duplicates_from_sorted_list_2', type: 'algorithm'},
+		{src: '/same_tree/1.jpg', desc: 'same_tree_1', type: 'algorithm'},
+		{src: '/same_tree/2.jpg', desc: 'same_tree_2', type: 'algorithm'},
+		{src: '/same_tree/3.jpg', desc: 'same_tree_3', type: 'algorithm'},
 		
     ]
 }
